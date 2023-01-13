e0cec178ed37f857511482e0dc8cf644275dfc79
smel.danik <smel.danik@gmail.com>
2023-01-13 15:50:09 +0300

    Добавлена корзина

---
+++ ClassLibraryMenu/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryMenu
+{
+    public class Order
+    {
+        int _id;
+        public int ID { get => _id; set => _id = value; }
+        Dictionary<Dish, int> Dishes = new Dictionary<Dish, int>();
+    }
+}
